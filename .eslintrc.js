ddae2202b8bc776311e7706b53cc240a0047860a
alchain <alchaincyf@gmail.com>
2025-04-05 13:12:13 +0800

    2

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['next/core-web-vitals'],
+  rules: {
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/no-unescaped-entities': 'off'
+  }
+} 
